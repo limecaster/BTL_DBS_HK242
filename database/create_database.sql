221ca514523267e8ec612012d7ec23f52c7ab454
limecaster <bang.dovan38@gmail.com>
2024-11-13 10:12:51 +0700

    change create database

--- database/create_database.sql
+++ database/create_database.sql
@@ -1,7 +1,17 @@
 -- Microsoft SQL Server 2022
 
+-- DROP Database
+USE master;
+GO
+DROP DATABASE bakery;
+GO
+
+-- CREATE database
 CREATE DATABASE bakery;
+GO
 USE bakery;
+GO
+
 
 CREATE TABLE Branch (
     Name NVARCHAR(255) PRIMARY KEY,
@@ -9,17 +19,20 @@ CREATE TABLE Branch (
     Phone NVARCHAR(255) NOT NULL,
     OpenHour TIME,
     CloseHour TIME,
-    OpenDate DATE
+    OpenDate DATE,
+	Status INT
 );
 
 CREATE TABLE EmployeeType (
     ID NVARCHAR(2) PRIMARY KEY,
     JobName NVARCHAR(50) NOT NULL,
+	Status INT
 )   
 
 CREATE TABLE EmployeePhone (
     ID INT IDENTITY(1, 1) PRIMARY KEY,
     Phone CHAR(10) NOT NULL,
+	Status INT
 )
 
 CREATE TABLE Employee (
@@ -32,6 +45,7 @@ CREATE TABLE Employee (
 	EPhoneID INT NOT NULL,
 	ETypeID NVARCHAR(2) NOT NULL,
 	BranchName NVARCHAR(255),
+	Status INT,
 
 	CONSTRAINT fk_emp_emp_phone FOREIGN KEY (EPhoneID)		REFERENCES EmployeePhone(ID),
 	CONSTRAINT fk_emp_emp_type	FOREIGN KEY (ETypeID)		REFERENCES EmployeeType(ID),
@@ -42,7 +56,8 @@ CREATE TABLE Membership (
 	ID INT IDENTITY(1, 1) PRIMARY KEY,
 	Name NVARCHAR(50),
 	Threshold INT,
-	DiscountPercent DECIMAL(5, 2)
+	DiscountPercent DECIMAL(5, 2),
+	Status INT
 )
 
 CREATE TABLE CustomerAddress (
@@ -50,7 +65,8 @@ CREATE TABLE CustomerAddress (
 	ApartmentNo NVARCHAR(200),
 	Street NVARCHAR(200),
 	District NVARCHAR(200),
-	City NVARCHAR(200)
+	City NVARCHAR(200),
+	Status INT
 )
 
 CREATE TABLE Customer (
@@ -62,6 +78,7 @@ CREATE TABLE Customer (
 	MembershipPoint INT,
 	MembershipID INT,
 	Password VARCHAR(255),
+	Status INT,
 
 	CONSTRAINT fk_cus_membership	FOREIGN KEY (MembershipID)	REFERENCES Membership(ID),
 	CONSTRAINT fk_cus_cus_address	FOREIGN KEY (AddressID)		REFERENCES CustomerAddress(ID),
@@ -70,7 +87,8 @@ CREATE TABLE Customer (
 CREATE TABLE Ingredient (
 	ID INT IDENTITY(1, 1) PRIMARY KEY,
 	Name NVARCHAR(255),
-	ImportPrice Money
+	ImportPrice Money,
+	Status INT
 )
 
 CREATE TABLE Cake (
@@ -82,15 +100,17 @@ CREATE TABLE Cake (
 	IsOther BIT,
 	IsOrder BIT,
 	CustomerNote NTEXT,
+	Status INT
 )
 
 CREATE TABLE Bill (
-	ID INT IDENTITY(1, 1),
+	ID INT IDENTITY(1, 1) PRIMARY KEY,
 	ReceiveAddress NVARCHAR(255),
 	ReceiveMONEY MONEY,
 	Date DATETIME,
 	CashierID CHAR(9) NOT NULL,
 	CustomerPhone CHAR(10) NOT NULL,
+	Status INT,
 
 	CONSTRAINT fk_bill_cus	FOREIGN KEY (CustomerPhone)	REFERENCES	Customer(Phone),
 	CONSTRAINT fk_bill_emp	FOREIGN KEY (CashierID)		REFERENCES	Employee(ID)
@@ -103,12 +123,14 @@ CREATE TABLE TotalBillCoupon (
 	DiscountPercent DECIMAL(5, 2),
 	DiscountMax DECIMAL(5, 2),
 	StartDate DATETIME,
-	EndDate DATETIME
+	EndDate DATETIME,
+	Status INT
 )
 
 CREATE TABLE BillApplyTotalBillCoupon (
 	BillID INT,
 	CouponID INT,
+	Status INT,
 
 	PRIMARY KEY (BillID, CouponID),
 
@@ -122,6 +144,7 @@ CREATE TABLE PerProductCoupon (
 	StartDate DATETIME,
 	EndDate DATETIME,
 	CakeID INT NOT NULL,
+	Status INT,
 
 	CONSTRAINT fk_perproductcoupon_cake FOREIGN KEY (CakeID) REFERENCES Cake(ID)
 )
@@ -129,6 +152,7 @@ CREATE TABLE PerProductCoupon (
 CREATE TABLE BillApplyPerProductCoupon (
 	BillID INT,
 	CouponID INT,
+	Status INT,
 
 	PRIMARY KEY (BillID, CouponID),
 
@@ -139,14 +163,15 @@ CREATE TABLE BillApplyPerProductCoupon (
 CREATE TABLE Decoration (
 	ID INT IDENTITY(1, 1) PRIMARY KEY,
 	Name NVARCHAR(255),
-	Price MONEY
+	Price MONEY,
+	Status INT
 )
 
 CREATE TABLE BillBonusDecoration (
 	BillID INT,
 	DecorationID INT,
-
 	Amount INT,
+	Status INT,
 
 	PRIMARY KEY (BillID, DecorationID),
 
@@ -157,8 +182,8 @@ CREATE TABLE BillBonusDecoration (
 CREATE TABLE BillHasCake (
 	BillID INT,
 	CakeID INT,
-
 	Amount INT,
+	Status INT,
 
 	PRIMARY KEY (BillID, CakeID),
 
@@ -169,8 +194,8 @@ CREATE TABLE BillHasCake (
 CREATE TABLE ComboCake (
 	CakeID1 INT,
 	CakeID2 INT,
-
 	Price Money,
+	Status INT,
 
 	PRIMARY KEY (CakeID1, CakeID2),
 
@@ -183,18 +208,21 @@ CREATE TABLE CakeHasIngredient (
 	IngredientID INT NOT NULL,
 	Amount INT,
 	Unit VARCHAR(10),
+	Status INT,
 
 	CONSTRAINT fk_cakehasingredient_cake		FOREIGN KEY (CakeID)		REFERENCES Cake(ID),
 	CONSTRAINT fk_cakehasingredient_ingredient	FOREIGN KEY (IngredientID)	REFERENCES Ingredient(ID)
 )
 
 CREATE TABLE WorkDay (
-	WeekDay INT PRIMARY KEY
+	WeekDay INT PRIMARY KEY,
+	Status INT,
 )
 
 CREATE TABLE Shift (
 	WeekDay INT,
 	StartHour DATETIME,
+	Status INT,
 
 	PRIMARY KEY (WeekDay, StartHour),
 
@@ -204,9 +232,10 @@ CREATE TABLE Shift (
 CREATE TABLE Register (
 	EID CHAR(9) NOT NULL,
 	WeekDay INT,
-	StartHour DATETIME
+	StartHour DATETIME,
+	Status INT,
 
-	PRIMARY KEY (EID, WeekDay, StartHour)
+	PRIMARY KEY (EID, WeekDay, StartHour),
 
 	CONSTRAINT fk_register_emp		FOREIGN KEY (EID)					REFERENCES	Employee(ID),
 	CONSTRAINT fk_register_shift	FOREIGN KEY (WeekDay, StartHour)	REFERENCES	Shift(WeekDay, StartHour)
@@ -217,6 +246,7 @@ CREATE TABLE WorksIn (
 	WeekDay INT,
 	StartHour DATETIME,
 	Date DATETIME,
+	Status INT,
 
 	PRIMARY KEY (EID, WeekDay, StartHour, Date),
 
